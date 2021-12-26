1c39bb39cca5714290de84a7406b9eef400b3ce8
Kalidas R <laki3824@gmail.com>
2021-12-25 22:47:05 -0600

    Limit service local config setup from remote repo

---
+++ src/main/java/com/example/microservices/limitsservices/configuration/Configuration.java
@@ -0,0 +1,21 @@
+package com.example.microservices.limitsservices.configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+
+    private int minimum;
+    private int maximum;
+
+}
